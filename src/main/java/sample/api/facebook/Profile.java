10b7543277c2d6f3ad14102531a6bb76b5dbb9fb
Ahmet Yetgin <ahmetyetgin@Ahmet-Yetgin.local>
2019-07-19 15:26:28 +0200

    Initial sample implementation

---
+++ src/main/java/sample/api/facebook/Profile.java
@@ -0,0 +1,14 @@
+package sample.api.facebook;
+
+import lombok.Data;
+
+@Data
+public class Profile {
+
+	//private String id;
+	private String name;
+	private String email;
+	private String first_name;
+	private String last_name;
+	//private String middle_name;
+}
